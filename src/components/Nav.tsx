662ebfa0ba057d2cbc8208fd0e4b85e8baeff78a
slTrust <trustfor@sina.cn>
2020-11-02 00:43:41 +0800

    创建icon组件

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Link } from "react-router-dom";
-require('icons/money.svg');
-require('icons/tag.svg');
-require('icons/chart.svg');
+import Icon from './icon';
 
 const NavWrapper = styled.nav`
   > ul{
@@ -31,21 +29,15 @@ const Nav = () => {
     <NavWrapper>
       <ul>
         <li>
-          <svg className="icon">
-            <use xlinkHref="#tag" />
-          </svg>
+          <Icon name="tag" />
           <Link to="/tags">标签页</Link>
         </li>
         <li>
-          <svg className="icon">
-            <use xlinkHref="#money" />
-          </svg>
+          <Icon name="money" />
           <Link to="/money">记账页</Link>
         </li>
         <li>
-          <svg className="icon">
-            <use xlinkHref="#chart" />
-          </svg>
+          <Icon name="chart" />
           <Link to="/statistics">统计页</Link>
         </li>
       </ul>
