1f91dc83e17d537ac0251d9260b3211fcc42e26b
slTrust <trustfor@sina.cn>
2020-11-01 23:40:04 +0800

    搞定三个icon

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Link } from "react-router-dom";
-import x from 'icons/money.svg';
-console.log(x);
+require('icons/money.svg');
+require('icons/tag.svg');
+require('icons/chart.svg');
 
 const NavWrapper = styled.nav`
   > ul{
@@ -12,7 +13,15 @@ const NavWrapper = styled.nav`
     > li {
       width:33.333333%;
       text-align:center;
-      padding:16px;
+      padding:4px 0;
+      display:flex;
+      flex-direction:column;
+      justify-content:center;
+      align-items:center;
+      .icon{
+        width: 24px;
+        height: 24px;
+      }
     }
   }
 `;
@@ -22,17 +31,25 @@ const Nav = () => {
     <NavWrapper>
       <ul>
         <li>
-          <img src={x} alt="" />
+          <svg className="icon">
+            <use xlinkHref="#tag" />
+          </svg>
           <Link to="/tags">标签页</Link>
         </li>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#money" />
+          </svg>
           <Link to="/money">记账页</Link>
         </li>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#chart" />
+          </svg>
           <Link to="/statistics">统计页</Link>
         </li>
       </ul>
-    </NavWrapper>
+    </NavWrapper >
   )
 }
 
