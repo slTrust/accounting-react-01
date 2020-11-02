67f3f68394c64c4b9bae105fe71a8938e619988d
slTrust <trustfor@sina.cn>
2020-11-02 12:31:56 +0800

    money.tsx之 备注 UI完成

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,10 +4,12 @@ import { NavLink } from "react-router-dom";
 import Icon from './Icon';
 
 const NavWrapper = styled.nav`
+  background:white;
+  line-height:24px;
+  box-shadow:0 0 3px rgba(0,0,0,0.25);
   > ul{
     display:flex;
-    line-height:24px;
-    box-shadow:0 0 3px rgba(0,0,0,0.25);
+   
     > li {
       width:33.333333%;
       text-align:center;
