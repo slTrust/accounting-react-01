b40aa44cb0a89cefe2861a1c380905cd00bca32b
slTrust <trustfor@sina.cn>
2020-11-02 13:34:44 +0800

    money.tsx UI完成

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -5,6 +5,11 @@ import styled from 'styled-components';
 const TagSection = styled.section`
   background:#fff;
   padding:12px 16px;
+  flex-grow:1;
+  display:flex;
+  flex-direction:column;
+  justify-content:flex-end;
+  align-items:flex-start;
   > ol {
     margin:0 -12px;
     > li {
@@ -126,11 +131,15 @@ const NumberPadSection = styled.section`
   }
 `;
 
+const MyLayout = styled(Layout)`
+  display:flex;
+  flex-direction:column;
+`;
 
 
 function Money() {
   return (
-    <Layout>
+    <MyLayout className="hi">
       <TagSection>
         <ol>
           <li>衣</li>
@@ -171,7 +180,7 @@ function Money() {
           <button className="dot">.</button>
         </div>
       </NumberPadSection>
-    </Layout>
+    </MyLayout>
   );
 }
 export default Money;
