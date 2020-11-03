c80c04dd038fb397df7f2cf07f80bbcc70ac8bbe
slTrust <trustfor@sina.cn>
2020-11-04 07:37:49 +0800

    抽离 useTags 函数(自定义hook的范例)

--- src/views/Money/TagSection.tsx
+++ src/views/Money/TagSection.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import { useTags } from 'useTags';
 
 const Wrapper = styled.section`
   background:#fff;
@@ -38,7 +39,7 @@ type Props = {
   onChange: (selected: string[]) => void;
 }
 const TagSection: React.FC<Props> = (props) => {
-  const [tags, setTags] = useState<string[]>(['衣', '食', '住', '行']);
+  const { tags, setTags } = useTags();
   const selectedTags = props.value;
 
   const onAddTag = () => {
