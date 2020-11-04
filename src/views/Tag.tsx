79aa42a62289b0b2fb61436ca5c52b59bfda5c2e
slTrust <trustfor@sina.cn>
2020-11-04 13:45:02 +0800

    创建单独的 Button 组件

--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -1,6 +1,19 @@
 import React from 'react';
 import { useTags } from '../useTags';
 import { useParams } from 'react-router-dom';
+import Layout from 'components/Layout';
+import Icon from 'components/Icon';
+import Button from 'components/Button';
+import styled from 'styled-components';
+
+const TopBar = styled.header`
+  display:flex;
+  justify-content:space-between;
+  align-items:center;
+  line-height:20px;
+  padding:14px;
+  background:white;
+`;
 
 type Params = {
   id: string
@@ -10,7 +23,22 @@ const Tag: React.FC = (props) => {
   let { id } = useParams<Params>();
   const tag = findTag(parseInt(id));
   return (
-    <div>{tag.name}</div>
+    <Layout>
+      <TopBar>
+        <Icon name="left" />
+        <span>编辑标签</span>
+        <Icon />
+      </TopBar>
+      <div>
+        <label>
+          <span>标签名</span>
+          <input type="text" placeholder="标签名" />
+        </label>
+      </div>
+      <div>
+        <Button>删除标签</Button>
+      </div>
+    </Layout>
   );
 };
 
