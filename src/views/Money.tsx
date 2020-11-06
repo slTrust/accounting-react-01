ff39468e9731ae9db259e870b598b57f8b9d1ada
slTrust <trustfor@sina.cn>
2020-11-06 15:32:20 +0800

    add record

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -5,6 +5,7 @@ import { TagSection } from './Money/TagSection';
 import { NoteSection } from './Money/NoteSection';
 import { CategorySection } from './Money/CategorySection';
 import { NumberPadSection } from './Money/NumberPadSection';
+import { useRecords } from '../hooks/useRecords';
 
 const MyLayout = styled(Layout)`
   display:flex;
@@ -13,20 +14,31 @@ const MyLayout = styled(Layout)`
 
 type Category = '-' | '+';
 
+const defaultFormData = {
+  tagIds: [] as number[],
+  note: '',
+  category: '-' as Category,
+  amount: 0
+};
+
 function Money() {
-  const [selected, setSelected] = useState({
-    tagIds: [] as number[],
-    note: '',
-    category: '-' as Category,
-    amount: 0
-  });
+  const [selected, setSelected] = useState(defaultFormData);
+  const { records, addRecord } = useRecords();
+
+
   type Selected = typeof selected;
   // Partial<Selected>  部分的 Selected 属性，可能含有 一个 两个、 多个
   const onChange = (obj: Partial<Selected>) => {
     setSelected({ ...selected, ...obj });
   };
+
+  const submit = () => {
+    addRecord(selected);
+    setSelected(defaultFormData);
+  }
   return (
     <MyLayout>
+      {JSON.stringify(selected)}
       <TagSection value={selected.tagIds}
         onChange={tagIds => onChange({ tagIds })} />
       <NoteSection value={selected.note}
@@ -35,7 +47,7 @@ function Money() {
         onChange={category => onChange({ category })} />
       <NumberPadSection value={selected.amount}
         onChange={amount => onChange({ amount })}
-        onOk={() => { }}
+        onOk={submit}
       />
     </MyLayout>
   );
